2772c9c1f3103e80ac1eb4aed610e6366141b5fc
jensborch <jens.borch@gmail.com>
2018-02-08 22:56:50 +0100

    Added more DB code

--- main.go
+++ main.go
@@ -4,12 +4,25 @@ import (
 	"fmt"
 
 	"github.com/jensborch/go-foosball/model"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 func main() {
+	db, err := gorm.Open("sqlite3", "/tmp/test.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
 	table1 := model.NewTable("1", model.Color{Right: "red", Left: "green"})
 	table2 := model.NewTable("2", model.Color{Right: "black", Left: "blue"})
 	tournament := model.NewTournament("Foosball tournament 1", []*model.Table{table1, table2})
+
+	db.Create(&tournament)
+
+	db.Where("TournamentID = ?", tournament.TournamentID).First(&tournament)
+
 	fmt.Println(tournament)
 
 	p1 := model.Player{
