8570b77b3722653406b842f2848c1be9797d0887
Jens Borch Christiansen <jens.borch@gmail.com>
2018-02-19 16:21:52 +0100

    Updated test

--- model/game_test.go
+++ model/game_test.go
@@ -4,16 +4,22 @@ import (
 	"testing"
 )
 
-func TestCreateGame(t *testing.T) {
-	table1 := NewTable("1", Color{Right: "red", Left: "green"})
-	table2 := NewTable("2", Color{Right: "black", Left: "blue"})
-	tournament := NewTournament("Foosball tournament 1", table1, table2)
-
+func initSingleGame(tournament *Tournament) *Game {
 	p1 := NewPlayer("tt", "Thomas")
-
 	p2 := NewPlayer("jj", "Jens")
+	return NewSinglesGame(tournament.TournamentTables[0], p1, p2)
+}
 
-	g := NewSinglesGame(tournament.TournamentTables[0], p1, p2)
+func initDuroGame(tournament *Tournament) *Game {
+	p1 := NewPlayer("tt", "Thomas")
+	p2 := NewPlayer("jj", "Jens")
+	p3 := NewPlayer("rr", "Rikke")
+	p4 := NewPlayer("kk", "Kristine")
+	return NewDuroGame(tournament.TournamentTables[0], PlayerPair{First: p1, Second: p2}, PlayerPair{First: p3, Second: p4})
+}
+
+func TestCreateSingleGame(t *testing.T) {
+	g := initSingleGame(InitTournament())
 
 	if len(g.UUID) != 36 {
 		t.Errorf("Player should have UUID, got: %s", g.UUID)
@@ -25,6 +31,26 @@ func TestCreateGame(t *testing.T) {
 
 	if g.Left()[0].RealName != "Jens" {
 		t.Errorf("Left player is incorrect, got: %s, want: %s.", g.Left()[0].RealName, "Jens")
+	}
+
+	if g.TournamentTable.Table.Name != "1" {
+		t.Errorf("Table is incorrect, got: %s, want: %s.", g.TournamentTable.Table.Name, "1")
+	}
+}
+
+func TestCreateDuroGame(t *testing.T) {
+	g := initDuroGame(InitTournament())
+
+	if len(g.UUID) != 36 {
+		t.Errorf("Player should have UUID, got: %s", g.UUID)
+	}
+
+	if g.Right()[1].RealName != "Jens" {
+		t.Errorf("Right player is incorrect, got: %s, want: %s.", g.Right()[1].RealName, "Jens")
+	}
+
+	if g.Left()[1].RealName != "Kristine" {
+		t.Errorf("Left player is incorrect, got: %s, want: %s.", g.Left()[1].RealName, "Kristine")
 	}
 
 	if g.TournamentTable.Table.Name != "1" {
--- model/tournament_test.go
+++ model/tournament_test.go
@@ -6,21 +6,21 @@ import (
 
 const TOURNAMENT = "T1"
 
-func initTournament() *Tournament {
+func InitTournament() *Tournament {
 	table1 := NewTable("1", Color{Right: "red", Left: "green"})
 	table2 := NewTable("2", Color{Right: "black", Left: "blue"})
 	return NewTournament(TOURNAMENT, table1, table2)
 }
 
 func TestCreateTournament(t *testing.T) {
-	tournament := initTournament()
+	tournament := InitTournament()
 	if tournament.Name != TOURNAMENT {
 		t.Errorf("Tournament name is incorrect, got: %s, want: %s.", tournament.Name, TOURNAMENT)
 	}
 }
 
 func TestAddTables2Tournament(t *testing.T) {
-	tournament := initTournament()
+	tournament := InitTournament()
 	tournament.AddTables(NewTable("3", Color{Right: "black", Left: "blue"}))
 
 	if len(tournament.TournamentTables) != 3 {
