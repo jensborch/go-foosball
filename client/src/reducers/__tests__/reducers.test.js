b8ef4878185a3483ce235967ff845854c79a5d85
Jens Borch Christiansen <jens.borch@gmail.com>
2018-03-17 10:18:28 +0100

    Added test

--- client/src/reducers/__tests__/reducers.test.js
+++ client/src/reducers/__tests__/reducers.test.js
@@ -1,7 +1,14 @@
-import { active } from '../reducers';
+import { active, players } from '../reducers';
 import { receiveTournamentPlayers } from '../../actions/actions';
 
-test('Tournament should contain active player', () => {
+test('Players should contain all player', () => {
+  const action = receiveTournamentPlayers(42, [{ nickname: 'key', realname: 'value' }]);
+  const state = players({}, action);
+  expect(state['key'].realname).toBe('value');
+});
+
+
+test('Active should contain active player', () => {
   const action = receiveTournamentPlayers(42, [{ nickname: 'name' }]);
   const state = active({}, action);
   expect(state[42]).toContain('name');
