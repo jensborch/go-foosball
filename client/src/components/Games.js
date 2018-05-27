2c5e616100a466a38a78ed88a8db0473376b5704
Jens Borch Christiansen <jens.borch@gmail.com>
2018-05-27 09:26:51 +0200

    Fixed additional import

--- client/src/components/Games.js
+++ client/src/components/Games.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Card, { CardContent } from '@material-ui/core/Card';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import withRoot from '../withRoot';
