898ca2d21eb0960a74de746f5a5dbb48f839f088
Jens Borch Christiansen <jens.borch@gmail.com>
2018-08-07 07:52:21 +0200

    Making it possible to change rating when adding player

---
+++ client/src/reducers/ranking.js
@@ -0,0 +1,32 @@
+import { types } from './tournaments';
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case types.RECEIVE_TOURNAMET_PLAYERS:
+      const newplayers = action.players.reduce(
+        (a, p) => ({
+          ...a,
+          [p.nickname]: p.ranking,
+        }),
+        {}
+      );
+      return {
+        ...state,
+        [action.id]: newplayers,
+      };
+    default:
+      return state;
+  }
+};
+
+export function getPlayerRanking(state, tournament, player) {
+  return state.ranking && state.ranking[tournament]
+    ? state.ranking[tournament][player]
+    : undefined;
+}
+
+export function getTournamentRanking(state, tournament) {
+  return state.tournaments && state.tournaments[tournament]
+    ? state.tournaments[tournament].initial
+    : undefined;
+}
