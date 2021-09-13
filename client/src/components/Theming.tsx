f29c25387519af9c4f02774a7d5bedfbef9d24aa
Jens Borch Christiansen <jens.borch@gmail.com>
2021-09-13 22:13:18 +0200

    Added theming

---
+++ client/src/components/Theming.tsx
@@ -0,0 +1,28 @@
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#62727b",
+      main: "#37474f",
+      dark: "#102027",
+      contrastText: "#ffffff",
+    },
+    secondary: {
+      light: "#ff833a",
+      main: "#e65100",
+      dark: "#ac1900",
+      contrastText: "#eeeeee",
+    },
+  },
+});
+
+export interface Props {
+  children: any;
+}
+
+function Theming(props: Props) {
+  return <ThemeProvider theme={theme}>{props.children}</ThemeProvider>;
+}
+
+export default Theming;
