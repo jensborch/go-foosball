213fcbcf56fac9388e1fab3049cbf1807494b55f
Jens Borch Christiansen <jens.borch@gmail.com>
2022-02-18 13:00:03 +0100

    Updated Swagger

--- resources/tables.go
+++ resources/tables.go
@@ -11,7 +11,15 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-// GetTable get info about a tables
+// GetTable get info about a table
+// @Summary      Get table
+// @Accept       json
+// @Produce      json
+// @Param        id       path      string  true  "Table ID"
+// @Success      200      {object}  model.Table
+// @Failure      404      {object}  ErrorResponse
+// @Failure      500      {object}  ErrorResponse
+// @Router       /tables/{id} [get]
 func GetTable(param string, db *gorm.DB) func(*gin.Context) {
 	return func(c *gin.Context) {
 		id := c.Param(param)
@@ -19,16 +27,21 @@ func GetTable(param string, db *gorm.DB) func(*gin.Context) {
 			c.JSON(http.StatusOK, t)
 			return
 		} else if err == nil {
-			c.JSON(http.StatusNotFound, gin.H{"error": fmt.Sprintf("Could not find %s", id)})
+			c.JSON(http.StatusNotFound, NewErrorResponse(fmt.Sprintf("Could not find %s", id)))
 			return
 		} else {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			c.JSON(http.StatusInternalServerError, NewErrorResponse(err.Error()))
 			return
 		}
 	}
 }
 
 // GetTables get list of all tables
+// @Summary      Get all tables
+// @Accept       json
+// @Produce      json
+// @Success      200      {array}  model.Table
+// @Router       /tables [get]
 func GetTables(db *gorm.DB) func(*gin.Context) {
 	return func(c *gin.Context) {
 		c.JSON(http.StatusOK, persistence.NewTableRepository(db).FindAll())
@@ -36,6 +49,15 @@ func GetTables(db *gorm.DB) func(*gin.Context) {
 }
 
 // PostTable creats new table
+// @Summary      Create table
+// @Accept       json
+// @Produce      json
+// @Param        table    body      model.Table true  "The table"
+// @Success      200      {object}  model.Table
+// @Failure      400      {object}  ErrorResponse
+// @Failure      404      {object}  ErrorResponse
+// @Failure      500      {object}  ErrorResponse
+// @Router       /tables/ [post]
 func PostTable(db *gorm.DB) func(*gin.Context) {
 	return func(c *gin.Context) {
 		var table model.Table
@@ -46,25 +68,33 @@ func PostTable(db *gorm.DB) func(*gin.Context) {
 				tx.Commit()
 				c.JSON(http.StatusOK, t)
 			} else {
-				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+				c.JSON(http.StatusBadRequest, NewErrorResponse(err.Error()))
 				tx.Rollback()
 			}
 		} else {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			c.JSON(http.StatusBadRequest, NewErrorResponse(err.Error()))
 		}
 	}
 }
 
 // GetTournamentTables list tables in a tournament
+// @Summary      Get tables in a tournament
+// @Accept       json
+// @Produce      json
+// @Param        id       path      string  true  "Tournament ID"
+// @Success      200      {array}   model.TournamentTable
+// @Failure      404      {object}  ErrorResponse
+// @Failure      500      {object}  ErrorResponse
+// @Router       /tournaments/{id}/tables [get]
 func GetTournamentTables(param string, db *gorm.DB) func(*gin.Context) {
 	return func(c *gin.Context) {
 		id := c.Param(param)
 		tournamentRepo := persistence.NewTournamentRepository(db)
 		if t, found, err := tournamentRepo.Find(id); !found {
-			c.JSON(http.StatusNotFound, gin.H{"error": fmt.Sprintf("Could not find tournament %s", id)})
+			c.JSON(http.StatusNotFound, NewErrorResponse(fmt.Sprintf("Could not find tournament %s", id)))
 			return
 		} else if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			c.JSON(http.StatusInternalServerError, NewErrorResponse(err.Error()))
 			return
 		} else {
 			c.JSON(http.StatusOK, t.TournamentTables)
@@ -79,6 +109,16 @@ type TableRepresentation struct {
 }
 
 // PostTournamentTables adds a table to a tournament
+// @Summary      Add table to tournament
+// @Accept       json
+// @Produce      json
+// @Param        id       path      string  true  "Tournament ID"
+// @Param        table    body      TableRepresentation true "The table"
+// @Success      200      {object}  model.TournamentTable
+// @Failure      400      {object}  ErrorResponse
+// @Failure      404      {object}  ErrorResponse
+// @Failure      500      {object}  ErrorResponse
+// @Router       /tournaments/{id}/tables [post]
 func PostTournamentTables(param string, db *gorm.DB) func(*gin.Context) {
 	return func(c *gin.Context) {
 		id := c.Param(param)
@@ -90,24 +130,24 @@ func PostTournamentTables(param string, db *gorm.DB) func(*gin.Context) {
 			err            error
 		)
 		if err = c.ShouldBindWith(&representation, binding.JSON); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			c.JSON(http.StatusBadRequest, NewErrorResponse(err.Error()))
 			return
 		}
 		tx := db.Begin()
 		r := persistence.NewTournamentRepository(tx)
 		if tournament, found, err = r.Find(id); !found {
-			c.JSON(http.StatusNotFound, gin.H{"error": fmt.Sprintf("Could not find tournament %s", tournament.Name)})
+			c.JSON(http.StatusNotFound, NewErrorResponse(fmt.Sprintf("Could not find tournament %s", tournament.Name)))
 			tx.Rollback()
 			return
 		}
 		if table, found, err = persistence.NewTableRepository(tx).Find(representation.UUID); !found {
-			c.JSON(http.StatusNotFound, gin.H{"error": fmt.Sprintf("Could not find table %s", representation.UUID)})
+			c.JSON(http.StatusNotFound, NewErrorResponse(fmt.Sprintf("Could not find table %s", representation.UUID)))
 			tx.Rollback()
 			return
 		}
 		tournament.AddTables(*table)
 		if err = r.Update(tournament); err != nil {
-			c.JSON(http.StatusNotFound, gin.H{"error": fmt.Sprintf("Could not add table to tournament %s", tournament.UUID)})
+			c.JSON(http.StatusNotFound, NewErrorResponse(fmt.Sprintf("Could not add table to tournament %s", tournament.UUID)))
 			tx.Rollback()
 			return
 		}
@@ -118,11 +158,20 @@ func PostTournamentTables(param string, db *gorm.DB) func(*gin.Context) {
 				return
 			}
 		}
-		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Could not find table %s in tournament %s", table.UUID, tournament.UUID)})
+		c.JSON(http.StatusInternalServerError, NewErrorResponse(fmt.Sprintf("Could not find table %s in tournament %s", table.UUID, tournament.UUID)))
 	}
 }
 
 // DeleteTournamentTable deletes a table from a tournament
+// @Summary      Remove table from tournament
+// @Accept       json
+// @Produce      json
+// @Param        id       path      string  true  "Tournament ID"
+// @Param        tableId  path      string  true  "Table ID"
+// @Success      204      {object}  ErrorResponse
+// @Failure      404      {object}  ErrorResponse
+// @Failure      500      {object}  ErrorResponse
+// @Router       /tournaments/{id}/tables/{tableId} [delete]
 func DeleteTournamentTable(tournamentParam string, tableParam string, db *gorm.DB) func(*gin.Context) {
 	return func(c *gin.Context) {
 		tourId := c.Param(tournamentParam)
@@ -132,14 +181,14 @@ func DeleteTournamentTable(tournamentParam string, tableParam string, db *gorm.D
 			if err := r.RemoveTable(t, tableId); err != nil {
 				c.Status(http.StatusNoContent)
 			} else {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				c.JSON(http.StatusInternalServerError, NewErrorResponse(err.Error()))
 			}
 			return
 		} else if err == nil {
-			c.JSON(http.StatusNotFound, gin.H{"error": fmt.Sprintf("Could not find tournament %s", tourId)})
+			c.JSON(http.StatusNotFound, NewErrorResponse(fmt.Sprintf("Could not find tournament %s", tourId)))
 			return
 		} else {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			c.JSON(http.StatusInternalServerError, NewErrorResponse(err.Error()))
 			return
 		}
 	}
