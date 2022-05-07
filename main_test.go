e45e58c6350ee3cbff6a251caaa4a43dd30ac634
Jens Borch Christiansen <jens.borch@gmail.com>
2022-05-07 16:16:48 +0200

    Updated tests

--- main_test.go
+++ main_test.go
@@ -89,38 +89,45 @@ func TestGetNotFound(t *testing.T) {
 	}
 }
 
-func TestPostPlayers(t *testing.T) {
+func postPlayers(ts *httptest.Server) func(t *testing.T) {
+	return func(t *testing.T) {
+
+		user1, err := json.Marshal(map[string]string{
+			"nickname": "u1",
+			"realname": "test1",
+			"rfid":     "string",
+		})
+
+		if err != nil {
+			t.Fatalf("Expected no error, got %v", err)
+		}
+
+		user2, err := json.Marshal(map[string]string{
+			"nickname": "u2",
+			"realname": "test2",
+		})
+
+		if err != nil {
+			t.Fatalf("Expected no error, got %v", err)
+		}
+
+		resp1, _ := http.Post(fmt.Sprintf("%s/players", ts.URL), "application/json", bytes.NewBuffer(user1))
+
+		if resp1.StatusCode != 201 {
+			t.Fatalf("Expected status code 201, got %v", resp1.StatusCode)
+		}
+
+		resp2, _ := http.Post(fmt.Sprintf("%s/players", ts.URL), "application/json", bytes.NewBuffer(user2))
+
+		if resp2.StatusCode != 201 {
+			t.Fatalf("Expected status code 201, got %v", resp2.StatusCode)
+		}
+	}
+}
+
+func Test(t *testing.T) {
 	ts, _ := startServer()
 	defer ts.Close()
 
-	user1, err := json.Marshal(map[string]string{
-		"nickname": "u1",
-		"realname": "test1",
-		"rfid":     "string",
-	})
-
-	if err != nil {
-		t.Fatalf("Expected no error, got %v", err)
-	}
-
-	user2, err := json.Marshal(map[string]string{
-		"nickname": "u2",
-		"realname": "test2",
-	})
-
-	if err != nil {
-		t.Fatalf("Expected no error, got %v", err)
-	}
-
-	resp1, _ := http.Post(fmt.Sprintf("%s/players", ts.URL), "application/json", bytes.NewBuffer(user1))
-
-	if resp1.StatusCode != 201 {
-		t.Fatalf("Expected status code 201, got %v", resp1.StatusCode)
-	}
-
-	resp2, _ := http.Post(fmt.Sprintf("%s/players", ts.URL), "application/json", bytes.NewBuffer(user2))
-
-	if resp2.StatusCode != 201 {
-		t.Fatalf("Expected status code 201, got %v", resp2.StatusCode)
-	}
+	postPlayers(ts)(t)
 }
