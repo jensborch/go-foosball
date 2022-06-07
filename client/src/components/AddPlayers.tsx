b6ca0d665fd5712de7ee47c249ec8e30b507115d
Jens Borch Christiansen <jens.borch@gmail.com>
2022-06-07 07:13:25 +0200

    Added styling

--- client/src/components/AddPlayers.tsx
+++ client/src/components/AddPlayers.tsx
@@ -2,32 +2,44 @@ import { Button, Grid, Modal, Paper } from '@mui/material';
 import PlayersGrid from './PlayersGrid';
 
 type AddPlayersProps = {
-  tournament: string
-  open: boolean
-  setOpen: (open: boolean) => void
-}
+  tournament: string;
+  open: boolean;
+  setOpen: (open: boolean) => void;
+};
 
-const AddPlayers = ({tournament, open, setOpen}: AddPlayersProps) => {
+const AddPlayers = ({ tournament, open, setOpen }: AddPlayersProps) => {
   return (
-    <Modal open={open} onClose={() => setOpen(false)}>
+    <Modal
+      open={open}
+      onClose={() => setOpen(false)}
+      sx={{
+        //position: 'absolute' as 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+      }}
+    >
       <Paper>
         <Grid
-          sx={{ overflow: 'auto' }}
-          container={true}
+          //sx={{ overflow: 'auto', margin: (theme) => theme.spacing(2) }}
+          //spacing={10}
+          spacing={2}
+          container
           direction="column"
-          spacing={8}
         >
-          <PlayersGrid
-            tournament={tournament}
-          />
-          <Button
-            sx={{ margin: (theme) => theme.spacing(2) }}
-            variant="contained"
-            color="secondary"
-            onClick={() => setOpen(false)}
-          >
-            Dismiss
-          </Button>
+          <Grid item>
+            <PlayersGrid tournament={tournament} />
+          </Grid>
+          <Grid item>
+            <Button
+              sx={{ margin: (theme) => theme.spacing(2) }}
+              variant="contained"
+              color="secondary"
+              onClick={() => setOpen(false)}
+            >
+              Dismiss
+            </Button>
+          </Grid>
         </Grid>
       </Paper>
     </Modal>
