648e6e95afbc9f07e936eb2d29e8dad0878e1e00
Jens Borch Christiansen <jens.borch@gmail.com>
2022-10-08 15:18:37 +0200

    Added fonts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,10 @@ import React from "react";
 import Tournament from "./pages/Tournament";
 import { CssBaseline } from "@mui/material";
 import ErrorBoundary from "./components/ErrorBoundary";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const queryClient = new QueryClient();
 
