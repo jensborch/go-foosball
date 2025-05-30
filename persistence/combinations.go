a76c5656646ea59f23c25cb7bbb3021db0f36514
Jens Borch Christiansen <jens.borch@gmail.com>
2025-05-30 19:38:21 +0200

    Updated code for getting combinations

--- persistence/combinations.go
+++ persistence/combinations.go
@@ -1,6 +1,8 @@
 package persistence
 
 import (
+	"fmt"
+	"math/rand/v2"
 	"sort"
 	"sync"
 
@@ -16,11 +18,12 @@ var (
 type GameCombinations struct {
 	sync.Mutex
 	current int
-	games   [][]*model.Game
+	rounds  [][]*model.Game
 	players []*model.TournamentPlayer
 }
 
 func GetGameCombinationsInstance(tournamentId string) *GameCombinations {
+	fmt.Printf("Getting combinations instance for tournament %s\n", tournamentId)
 	once.Do(func() {
 		instance = make(map[string]*GameCombinations)
 	})
@@ -29,6 +32,7 @@ func GetGameCombinationsInstance(tournamentId string) *GameCombinations {
 
 	g, ok := instance[tournamentId]
 	if !ok {
+		fmt.Printf("Creating new instance for tournament %s\n", tournamentId)
 		g = &GameCombinations{}
 		instance[tournamentId] = g
 	}
@@ -39,24 +43,50 @@ func (c *GameCombinations) Next() []*model.Game {
 	c.Lock()
 	defer c.Unlock()
 	var result []*model.Game = nil
-	if len(c.games) != 0 {
-		if c.current >= len(c.games) {
+	if len(c.rounds) != 0 {
+		if c.current >= len(c.rounds) {
 			c.current = 0
 		}
-		result = c.games[c.current]
+		result = c.rounds[c.current]
 		c.current++
 	}
+	fmt.Printf("Playing round %d\n", c.current)
 	return result
 }
 
-func (c *GameCombinations) Update(players []*model.TournamentPlayer, tables []*model.TournamentTable) {
+func (c *GameCombinations) Randomize() {
+	c.Lock()
+	defer c.Unlock()
+	if c.rounds != nil {
+		result := make([][]*model.Game, len(c.rounds))
+		perm := rand.Perm(len(c.rounds))
+		for i, v := range perm {
+			result[v] = c.rounds[i]
+		}
+		c.rounds = result
+	}
+}
+
+func (c *GameCombinations) Update(players []*model.TournamentPlayer, tables []*model.TournamentTable) int {
 	c.Lock()
 	defer c.Unlock()
 	if !isSamePlayers(c.players, players) {
 		c.players = players
-		c.games = allGamePlayerCombinations(players, tables)
+		c.rounds = allGamePlayerCombinations(players, tables)
 		c.current = 0
+		fmt.Printf("Created new set of %d rounds from %d players\n", len(c.rounds), len(c.players))
+		return len(c.rounds)
 	}
+	return 0
+}
+
+func sortPlayersById(players []*model.TournamentPlayer) []*model.TournamentPlayer {
+	result := make([]*model.TournamentPlayer, len(players))
+	copy(result, players)
+	sort.Slice(result, func(i, j int) bool {
+		return players[i].ID < players[j].ID
+	})
+	return result
 }
 
 func isSamePlayers(players1, players2 []*model.TournamentPlayer) bool {
@@ -64,16 +94,11 @@ func isSamePlayers(players1, players2 []*model.TournamentPlayer) bool {
 		return false
 	}
 
-	// Sort the arrays
-	sort.Slice(players1, func(i, j int) bool {
-		return players1[i].ID < players1[j].ID
-	})
-	sort.Slice(players2, func(i, j int) bool {
-		return players2[i].ID < players2[j].ID
-	})
+	sortedPlayers1 := sortPlayersByNickname(players1)
+	sortedPlayers2 := sortPlayersByNickname(players2)
 
-	for i := range players1 {
-		if players1[i] != players2[i] {
+	for i := range sortedPlayers1 {
+		if sortedPlayers1[i].Player.Nickname != sortedPlayers2[i].Player.Nickname {
 			return false
 		}
 	}
@@ -120,6 +145,7 @@ func generatePlayerPairsCombinations(players []*model.TournamentPlayer) [][][]*m
 			combinations = append(combinations, combination)
 		}
 	}
+	fmt.Println("hej")
 	return combinations
 }
 
@@ -139,7 +165,6 @@ func allGamePlayerCombinations(players []*model.TournamentPlayer, tables []*mode
 
 	combinations := generatePlayerPairsCombinations(players)
 	numberOfCombinations := len(combinations)
-	//tablesCount := int(math.Floor(float64(len(tables)*4) / float64(len(players))))
 
 	for combinationIndex := 0; combinationIndex < numberOfCombinations; combinationIndex++ {
 		round := make([]*model.Game, 0)
@@ -148,7 +173,10 @@ func allGamePlayerCombinations(players []*model.TournamentPlayer, tables []*mode
 			if playersLeft < 2 {
 				break
 			}
-			for nextIndex := combinationIndex; nextIndex < numberOfCombinations; nextIndex++ {
+			for nextIndex := combinationIndex; ; nextIndex++ {
+				if nextIndex == numberOfCombinations {
+					nextIndex = 0
+				}
 				game := createGame(playersLeft, table, combinations[nextIndex])
 				if !hasSamePlayers(&game, round) {
 					round = append(round, &game)
