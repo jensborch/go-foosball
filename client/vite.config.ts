a8afbf88ccedb6486bdf4c6a47cc9df13fcafc7d
Jens Borch Christiansen <jens.borch@gmail.com>
2026-01-20 19:28:24 +0100

    Updated client dependencies

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: "jsdom",
+    environment: "happy-dom",
     setupFiles: "./src/setupTests.ts",
     css: true,
     reporters: ["default"],
